d65539101632d0a421d6963f314d9c1ada417a5b
Tien-MinhDuong <Tinduong2492@gmail.com>
2021-12-25 13:45:47 +0100

    Add project files.

---
+++ StringCalc/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Program
+{
+    public class StringCalc
+    {
+
+        public static int Add(String numbers)
+        {
+            int result;
+
+            if (string.IsNullOrEmpty(numbers))
+            {
+                Console.WriteLine(0);
+                return 0;
+            }
+
+            if (numbers.StartsWith("//"))
+            {
+                result = CalculateWithSpecifiedDelimiter(numbers);
+            }
+            else
+            {
+                result = Calculate(numbers);
+            }
+            Console.WriteLine(result);
+            return result;
+        }
+
+        private static int CalculateWithSpecifiedDelimiter(string input)
+        {
+            var delimiter = GetDelimiter(input);
+            input = input.Substring(input.IndexOf("\n"));
+            return Calculate(input, delimiter);
+        }
+
+        private static int Calculate(string input)
+        {
+            return Calculate(input, GetDelimiter(input));
+        }
+
+        private static int Calculate(String input, string[] delimiter)
+        {
+            var values = input.Split(delimiter, StringSplitOptions.None);
+            var intValues = values.Select(x => int.Parse(x)).Where(x => x < 1001);
+
+            CheckForNegativeNumbers(intValues);
+
+
+            return intValues.Sum();
+        }
+
+        private static void CheckForNegativeNumbers(IEnumerable<int> intValues)
+        {
+            var negatives = intValues.Where(x => x < 0).ToList();
+            var errorMessage = "negatives not allowed: ";
+
+            if (negatives.Count() > 0)
+            {
+                negatives.ForEach(x => errorMessage += x);
+                throw new Exception(errorMessage);
+            }
+        }
+
+        private static string[] GetDelimiter(String input)
+        {
+            string[] defaultDelimiter = new string[] { ",", "\n" };
+            if (input.StartsWith("//["))
+            {
+                return GetMultipleDelimiters(input);
+            }
+            if (input.StartsWith("//"))
+            {
+                return new string[] { input[2].ToString() };
+            }
+
+            return defaultDelimiter;
+        }
+
+        public static string[] GetMultipleDelimiters(String input)
+        {
+
+            input = input.TrimStart("/[".ToCharArray());
+            input = input.Split(new[] { "]\n" }, StringSplitOptions.None).First();
+            return input.Split(new[] { "][" }, StringSplitOptions.None);
+
+        }
+    }
+
+}
+
---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,120 @@
+using Program;
+using System;
+using Xunit;
+
+namespace TestProject
+{
+    public class UnitTest1
+    {
+
+        private void CalculateString(string numbers, int expectedResult)
+        {
+            var result = StringCalc.Add(numbers);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void Given_Empty_String_Returns_Zero()
+        {
+            CalculateString("", 0);
+        }
+
+        [Fact]
+        public void Given_String_1_Returns_1()
+        {
+            CalculateString("1", 1);
+        }
+
+        [Fact]
+        public void Given_String_1_2_Returns_3()
+        {
+            CalculateString("1,2", 3);
+        }
+
+        [Fact]
+        public void Given_String_1_2_3_4_5_6_7_Returns_28()
+        {
+            CalculateString("1,2,3,4,5,6,7", 28);
+        }
+
+        [Fact]
+        public void Given_String_1_NewLine2_3_Returns_6()
+        {
+            CalculateString("1\n2,3", 6);
+        }
+
+        [Fact]
+        public void Given_Input_With_Delimeter_1_2_Returns_3()
+        {
+            CalculateString("//;\n1;2", 3);
+        }
+
+        [Fact]
+        public void Given_Negative_Number_Throw_Exception()
+        {
+            Assert.Throws<Exception>(() => { CalculateString("-1", 0); });
+        }
+
+        [Fact]
+        public void Given_Negative_Number_Throw_Exception_With_Message()
+        {
+            try
+            {
+                CalculateString("-1", 0);
+            }
+            catch (Exception ex)
+            {
+                Assert.Contains("negatives not allowed", ex.Message);
+                Assert.Contains("-1", ex.Message);
+            }
+
+        }
+
+        [Fact]
+        public void Given_Negative_Number_Throw_Exception_Specifying_Invalid_Numbers()
+        {
+            try
+            {
+                CalculateString("-1,-2", 0);
+            }
+            catch (Exception ex)
+            {
+                Assert.Contains("negatives not allowed", ex.Message);
+                Assert.Contains("-1", ex.Message);
+                Assert.Contains("-2", ex.Message);
+            }
+        }
+
+        [Fact]
+        public void Ignore_Number_Larger_Than_One_Thousand()
+        {
+            CalculateString("2,1001", 2);
+        }
+
+        [Fact]
+        public void Delimiter_Of_Any_Lenght()
+        {
+            CalculateString("//[***]\n1***2***3", 6);
+        }
+
+        [Fact]
+        public void Multiple_Delimiter_Of_Any_Lenght()
+        {
+            CalculateString("//[*][%]\n1*2%3", 6);
+        }
+
+        [Fact]
+        public void Multiple_Delimiter_Of_Multiple_Lenght()
+        {
+            CalculateString("//[**][%%]\n1**2%%3", 6);
+        }
+
+        [Fact]
+        public void Multiple_Delimiter_Yest()
+        {
+            var delimiters = StringCalc.GetMultipleDelimiters("//[*][%]\n1*2%3");
+            Assert.Contains("*", delimiters);
+            Assert.Contains("%", delimiters);
+        }
+    }
+}
